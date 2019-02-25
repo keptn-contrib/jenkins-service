5911757f22560c365145da3a9bc12a569def8a0c
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-02-25 13:22:19 +0100

    Removed github elements.

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 // server.js
 var express = require('express');
 var bodyParser = require('body-parser');
-const { base64encode, base64decode } = require('nodejs-base64');
 var app = express();
 
 // parse application/json
 app.use(bodyParser.json())
 
-var gitHubOperator = require('./operator.js').githubWebhookListener;
 var jenkinsOperator = require('./operator.js').jenkinsNotificationListener;
-
-app.post('/githubOperator', function(req, res) {
-  gitHubOperator(req, res);
-});
 
 app.post('/jenkinsOperator', jenkinsOperator);
 
@@ -20,15 +14,12 @@ app.post('/', function(req, res) {
   console.log(req);
   //const pubsubPayload = JSON.parse(base64decode(req.body.Data));
   const pubsubPayload = req.body;
-  if (pubsubPayload.channel === 'github') {
-    gitHubOperator(pubsubPayload, res)
-  } else if (pubsubPayload.channel === 'jenkins') {
+  if (pubsubPayload.channel === 'jenkins') {
     jenkinsOperator(pubsubPayload, res);
   }
 });
 
 app.get('/health', function (req, res, next) {
-  // check my health
   res.sendStatus(200)
 });
 
