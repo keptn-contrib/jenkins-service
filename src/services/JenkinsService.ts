7f36020f1d1784bcea38f680499992cd28c06bd3
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-03-05 15:43:02 +0100

    Add deploy service step.

--- src/services/JenkinsService.ts
+++ src/services/JenkinsService.ts
@@ -1,6 +1,7 @@
 import { DeploymentModel } from '../types/DeploymentModel';
 
-const jenkins = require('jenkins')({ baseUrl: process.env.JENKINS_URL });
+//const jenkins = require('jenkins')({ baseUrl: process.env.JENKINS_URL });
+const jenkins = require('jenkins')({ baseUrl: 'http://admin:AiTx4u8VyUV8tCKk@35.232.207.212:24711' });
 
 export class JenkinsService {
 
@@ -17,27 +18,35 @@ export class JenkinsService {
   }
 
   async deployService(deployment: DeploymentModel) : Promise<boolean> {
-    const updated: boolean = false;
+    const deployed: boolean = false;
 
-    jenkins.job.build({ name: `deploy`, parameters: {
-      SERVICE: deployment.service,
-      STAGE: deployment.stage,
-      PROJECT: 'sockshop',
-      GITHUB_ORG: 'keptn-tiger',
-    } });
+    new Promise(resolve => {
+      jenkins.job.build({
+        name: `/deploy`,
+        parameters: {
+          SERVICE: deployment.service,
+          PROJECT: 'sockshop',
+          STAGE: deployment.stage,
+          GITHUB_ORG: 'keptn-tiger',
+        },
+      }, function(err) {
+        if (err) console.log(err);
+        resolve();
+      });
+    });
 
-    return updated;
+    return deployed;
   }
 
   async startTests(deployment: DeploymentModel) : Promise<boolean> {
-    let updated: boolean = false;
+    let started: boolean = false;
 
-    return updated;
+    return started;
   }
 
   async evaluateTests(deployment: DeploymentModel) : Promise<boolean> {
-    let updated: boolean = false;
+    let evaluated: boolean = false;
 
-    return updated;
+    return evaluated;
   }
 }
