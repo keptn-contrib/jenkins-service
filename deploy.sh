701364d0f9cfb1b5c640b8bc4ae302a62f7b2ebe
Florian Bacher <bacherfl@gmail.com>
2019-03-13 11:26:56 +0100

    fixed path to jenkins deployment manifest

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ cat config/jenkins/k8s-jenkins-deployment.yml | \
   sed 's~GITHUB_ORGANIZATION_PLACEHOLDER~'"$GITHUB_ORGANIZATION"'~' | \
   sed 's~DOCKER_REGISTRY_IP_PLACEHOLDER~'"$REGISTRY_URL"'~' | \
   sed 's~DT_TENANT_URL_PLACEHOLDER~'"$DT_TENANT_URL"'~' | \
-  sed 's~DT_API_TOKEN_PLACEHOLDER~'"$DT_API_TOKEN"'~' >> ../manifests/gen/k8s-jenkins-deployment.yml
+  sed 's~DT_API_TOKEN_PLACEHOLDER~'"$DT_API_TOKEN"'~' >> config/jenkins/gen/k8s-jenkins-deployment.yml
 
 kubectl create -f config/jenkins/k8s-jenkins-pvcs.yml 
 kubectl create -f config/jenkins/gen/k8s-jenkins-deployment.yml
