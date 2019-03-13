c9826cf23d032952b272c9a6fb5bf7a7b6e7df3d
Florian Bacher <bacherfl@gmail.com>
2019-03-13 11:33:43 +0100

    delete previous service before deploying
    otherwise, it won't be rebuilt

--- deploy.sh
+++ deploy.sh
@@ -74,6 +74,7 @@ rm -f config/service/gen/service.yaml
 cat config/service/service.yaml | \
   sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/service/gen/service.yaml
 
+kubectl delete -f config/service/gen/service.yaml
 kubectl apply -f config/service/gen/service.yaml
 
 # Deploy Tiller for Helm
